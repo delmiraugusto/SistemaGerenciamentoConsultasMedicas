67fc76113ef8cef5dbefeb33c665c3822fcb246f
Delmir Augusto <delmir19.da@gmail.com>
2025-01-13 11:14:36 -0300

    Todos os Testes Funcionando

---
+++ SistemaDeGerenciamentoDeConsultasMedicas.Tests/Tests/DoctorTest.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using Sistema_de_Gerenciamento_de_Consultas_Médicas.Application.DTO;
+using Sistema_de_Gerenciamento_de_Consultas_Médicas.Application.ServiceApp;
+using Sistema_de_Gerenciamento_de_Consultas_Médicas.Controllers;
+using Sistema_de_Gerenciamento_de_Consultas_Médicas.Domain.Entities;
+using Sistema_de_Gerenciamento_de_Consultas_Médicas.Domain.IRepository;
+
+namespace SistemaDeGerenciamentoDeConsultasMedicas.Tests.Tests;
+
+public class DoctorTest
+{
+
+    private readonly Mock<IDoctorRepository> _mockDoctorRepository;
+    private readonly DoctorService _doctorService;
+
+    public DoctorTest()
+    {
+        _mockDoctorRepository = new Mock<IDoctorRepository>();
+        _doctorService = new DoctorService(_mockDoctorRepository.Object);
+    }
+
+    [Fact]
+    public async Task GetAllAsync_Test()
+    {
+        var doctors = new List<Doctor>
+        {
+            new Doctor { Id = 1, Name = "Dr. Delso", Email = "delso@gmail.com", Telephone = "123456789", Cpf = "123.456.789-00", Crm = "CRM123", PasswordHash = "passwordTest", Specialty = "Cardiologista", IsActive = true }
+        };
+
+        _mockDoctorRepository.Setup(repo => repo.GetAllAsync())
+            .ReturnsAsync(doctors);
+
+        var result = await _doctorService.GetAllAsync();
+
+        Assert.NotNull(result);
+        Assert.Single(result);
+        Assert.Equal("Dr. Delso", result.First().Name);
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_Test()
+    {
+        var doctor = new Doctor
+        {
+            Id = 1,
+            Name = "Dr. Delso",
+            Email = "delso@gmail.com",
+            Telephone = "123456789",
+            Cpf = "123.456.789-00",
+            Crm = "CRM123",
+            PasswordHash = "passwordTest",
+            Specialty = "Cardiologista",
+            IsActive = true
+        };
+
+        _mockDoctorRepository.Setup(repo => repo.GetByIdAsync(1))
+            .ReturnsAsync(doctor); 
+
+        var result = await _doctorService.GetByIdAsync(1);
+
+        Assert.NotNull(result);
+        Assert.Equal(1, result.Id);
+        Assert.Equal("Dr. Delso", result.Name);
+    }
+
+    [Fact]
+    public async Task GetByEmailAsync_Test()
+    {
+        var doctor = new Doctor
+        {
+            Id = 1,
+            Name = "Dr. João",
+            Email = "delso@gmail.com",
+            Telephone = "123456789",
+            Cpf = "123.456.789-00",
+            Crm = "CRM123",
+            PasswordHash = "password",
+            Specialty = "Cardiologista",
+            IsActive = true
+        };
+
+        _mockDoctorRepository.Setup(repo => repo.GetByEmailAsync("delso@gmail.com"))
+            .ReturnsAsync(doctor); 
+
+        var result = await _doctorService.GetByEmailAsync("delso@gmail.com");
+
+        Assert.NotNull(result);
+        Assert.Equal("delso@gmail.com", result.Email);
+    }
+
+    [Fact]
+    public async Task UpdateAsync_Test()
+    {
+        var doctorDTO = new Doctor
+        {
+            Id = 1,
+            Name = "Dr. delso",
+            Email = "delso@gmail.com",
+            Telephone = "987654321",
+            Cpf = "123.456.789-00",
+            Crm = "CRM123",
+            PasswordHash = "password",
+            Specialty = "Cardiologista",
+            IsActive = true
+        };
+
+        _mockDoctorRepository.Setup(repo => repo.GetByIdAsync(1))
+            .ReturnsAsync(doctorDTO);
+
+        _mockDoctorRepository.Setup(repo => repo.UpdateAsync(It.IsAny<Doctor>()))
+            .Returns(Task.CompletedTask);
+
+        await _doctorService.UpdateAsync(1, doctorDTO);
+
+        _mockDoctorRepository.Verify(repo => repo.UpdateAsync(It.IsAny<Doctor>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task CancelAsync_DeactivateDoctor_WhenHasNoConsults_Test()
+    {
+        var doctor = new Doctor
+        {
+            Id = 1,
+            Name = "Dr. Delso",
+            Email = "delso@gmail.com",
+            Telephone = "123456789",
+            Cpf = "123.456.789-00",
+            Crm = "CRM123",
+            PasswordHash = "password",
+            Specialty = "Cardiologista",
+            IsActive = true
+        };
+
+        _mockDoctorRepository.Setup(repo => repo.GetByIdAsync(1))
+            .ReturnsAsync(doctor);
+
+        _mockDoctorRepository.Setup(repo => repo.HasConsultsAsync(1))
+            .ReturnsAsync(false);
+
+        _mockDoctorRepository.Setup(repo => repo.UpdateAsync(It.IsAny<Doctor>()))
+            .Returns(Task.CompletedTask);
+
+        await _doctorService.CancelAsync(1);
+
+        Assert.False(doctor.IsActive);
+        _mockDoctorRepository.Verify(repo => repo.UpdateAsync(It.IsAny<Doctor>()), Times.Once);
+    }
+
+    [Fact]
+    public async Task CancelAsync_Exception_DoctorHasConsults_Test()
+    {
+        var doctor
+            = new Doctor
+        {
+            Id = 1,
+            Name = "Dr. Delso",
+            Email = "delso@gmail.com",
+            Telephone = "123456789",
+            Cpf = "123.456.789-00",
+            Crm = "CRM123",
+            PasswordHash = "password",
+            Specialty = "Cardiologista",
+            IsActive = true
+        };
+
+        _mockDoctorRepository.Setup(repo => repo.GetByIdAsync(1))
+            .ReturnsAsync(doctor);
+
+        _mockDoctorRepository.Setup(repo => repo.HasConsultsAsync(1))
+            .ReturnsAsync(true);
+
+        var exception = await Assert.ThrowsAsync<ApplicationException>(() => _doctorService.CancelAsync(1));
+        Assert.Equal("Não é possível desativar o médico, pois ele está associado a uma ou mais consultas.", exception.Message);
+    }
+
+}
---
+++ SistemaDeGerenciamentoDeConsultasMedicas.Tests/Tests/PatientTest.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Sistema_de_Gerenciamento_de_Consultas_Médicas.Application.DTO;
+using Sistema_de_Gerenciamento_de_Consultas_Médicas.Application.ServiceApp;
+using Sistema_de_Gerenciamento_de_Consultas_Médicas.Domain.Entities;
+using Sistema_de_Gerenciamento_de_Consultas_Médicas.Domain.IRepository;
+
+namespace SistemaDeGerenciamentoDeConsultasMedicas.Tests.Tests;
+
+public class PatientTest
+{
+    private readonly Mock<IPatientRepository> _mockPatientRepository;
+    private readonly PatientService _patientService;
+
+    public PatientTest()
+    {
+        _mockPatientRepository = new Mock<IPatientRepository>();
+        _patientService = new PatientService(_mockPatientRepository.Object);
+    }
+
+    [Fact]
+    public async Task GetAllAsync_Test()
+    {
+        var patients = new List<Patient>
+        {
+            new Patient { Id = 1, Name = "Delso Silva", Email = "delso@gmail.com", Telephone = "123456789", Cpf = "123.456.789-00", Age = "30" },
+            new Patient { Id = 2, Name = "Augusto Silva", Email = "augusto@gmail.com", Telephone = "987654321", Cpf = "987.654.321-00", Age = "28" }
+        };
+
+        _mockPatientRepository.Setup(repo => repo.GetAllAsync()).ReturnsAsync(patients);
+
+        var result = await _patientService.GetAllAsync();
+
+        Assert.NotEmpty(result);
+        Assert.Equal(2, result.Count());
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_Test()
+    {
+        var patient = new Patient { Id = 1, Name = "Delso Silva", Email = "delso@gmail.com", Telephone = "123456789", Cpf = "123.456.789-00", Age = "30" };
+
+        _mockPatientRepository.Setup(repo => repo.GetByIdAsync(1)).ReturnsAsync(patient);
+
+        var result = await _patientService.GetByIdAsync(1);
+
+        Assert.NotNull(result);
+        Assert.Equal(1, result.Id);
+    }
+
+    [Fact]
+    public async Task GetByEmailAsync_Test()
+    {
+        var patient = new Patient { Id = 1, Name = "Delso Silva", Email = "delso@gmail.com", Telephone = "123456789", Cpf = "123.456.789-00", Age = "30" };
+
+        _mockPatientRepository.Setup(repo => repo.GetByEmailAsync("delso@gmail.com")).ReturnsAsync(patient);
+
+        var result = await _patientService.GetByEmailAsync("delso@gmail.com");
+
+        Assert.NotNull(result);
+        Assert.Equal("delso@gmail.com", result.Email);
+    }
+
+
+    [Fact]
+    public async Task UpdateAsync_Test()
+    {
+        var patientDTO = new PatientDTO(
+            Id: 1,
+            Name: "Delso teste Up",
+            Email: "delsoupdated@gmail.com",
+            PasswordHash: "teste123",
+            Cpf: "123.456.789-00",
+            Age: "30",
+            Telephone: "123456789"
+        );
+
+        var patient = new Patient
+        {
+            Id = 1,
+            Name = "delso Silva",
+            Email = "delso@gmail.com",
+            Telephone = "123456789",
+            Cpf = "123.456.789-00",
+            Age = "30"
+        };
+
+        _mockPatientRepository.Setup(repo => repo.GetByIdAsync(1)).ReturnsAsync(patient);
+        _mockPatientRepository.Setup(repo => repo.UpdateAsync(It.IsAny<Patient>())).Returns(Task.CompletedTask);
+
+        await _patientService.UpdateAsync(1, patientDTO);
+
+        _mockPatientRepository.Verify(repo => repo.UpdateAsync(It.Is<Patient>(p => p.Name == "Delso teste Up")), Times.Once);
+    }
+
+
+    [Fact]
+    public async Task DeleteAsync_DeletePatient_Test()
+    {
+        var patient = new Patient { Id = 1, Name = "Delso Silva", Email = "delso@gmail.com", Telephone = "123456789", Cpf = "123.456.789-00", Age = "30" };
+
+        _mockPatientRepository.Setup(repo => repo.GetByIdAsync(1)).ReturnsAsync(patient);
+        _mockPatientRepository.Setup(repo => repo.HasConsultsAsync(1)).ReturnsAsync(false);
+        _mockPatientRepository.Setup(repo => repo.DeleteAsync(1)).Returns(Task.CompletedTask);
+
+        await _patientService.DeleteAsync(1);
+
+        _mockPatientRepository.Verify(repo => repo.DeleteAsync(1), Times.Once);
+    }
+
+    [Fact]
+    public async Task DeleteAsync_Exception_PatientHasConsults_Test()
+    {
+        var patient = new Patient { Id = 1, Name = "Delso Silva", Email = "delos@gmail.com", Telephone = "123456789", Cpf = "123.456.789-00", Age = "30" };
+
+        _mockPatientRepository.Setup(repo => repo.GetByIdAsync(1)).ReturnsAsync(patient);
+        _mockPatientRepository.Setup(repo => repo.HasConsultsAsync(1)).ReturnsAsync(true);
+
+        await Assert.ThrowsAsync<ApplicationException>(() => _patientService.DeleteAsync(1));
+    }
+
+
+}
